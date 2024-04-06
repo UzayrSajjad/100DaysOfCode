59b1ce59752b4dd489c34f442da18609b9309177
Uzair Sajjad <158592504+UzayrSajjad@users.noreply.github.com>
2024-04-07 00:31:05 +0500

    Minimum Remove to Make Valid Parentheses.cpp

---
+++ Day12/Minimum Remove to Make Valid Parentheses.cpp
@@ -0,0 +1,50 @@
+class Solution {
+public:
+    string minRemoveToMakeValid(string s) {
+        vector<char> str(s.size()); 
+        int n = s.length();
+        int count = 0;
+
+        for(int i=0; i<n; i++) {
+            str[i] = s[i];
+        }
+        
+        // First pass from 0 index
+        for(int i=0; i<n; i++) {
+            if(str[i] == '(' ) {
+                count++;
+            }
+            else if(str[i] == ')' ) {
+                if(count == 0) {
+                    str[i] = ' '; // Assign a character, not a string
+                } else {
+                    count--;
+                }
+            }
+        }
+
+        // Second pass from n-1
+        count = 0;
+        for(int i=n-1; i>=0; i--) {
+            if(str[i] == ')' ) {
+                count++;
+            }
+            else if(str[i] == '(' ) {
+                if(count == 0) {
+                    str[i] = ' '; 
+                } else {
+                    count--;
+                }
+            }
+        }
+
+        string result;
+
+        for(int i=0; i<n; i++) {
+            if(str[i] != ' ') {
+                result += str[i]; 
+            }
+        }
+        return result;
+    }
+};
